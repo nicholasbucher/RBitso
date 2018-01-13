c149b728ec8304a83918d43cd332b624595e145e
nicholas <bucher@ciencias.unam.mx>
2018-01-13 16:50:35 +0000

    Automatic Commit

--- examples/Money_Maker.R
+++ examples/Money_Maker.R
@@ -1,6 +1,6 @@
 # Money Maker
 
-
+library(RBitso)
 
 # Preliminar --------------------------------------------------------------
 
@@ -9,4 +9,35 @@
 
 # Antes de poder implementar un sistema así, es indispensable realizar algunas pruebas para conocer si es 
 # posible producir dicho programa, reconocer indicadores de compra-venta, observar margenes de tendencia etc.
+
+# 1. Correr código desde la consola R < Money_Maker.R --no-save
+# 2. Guardar precios por hora en csv
+
+setwd("Test")
+Today<-Today<-gsub("-","_",Sys.Date())
+if(is.na(match(Today,system("ls",intern=TRUE)))){
+  dir.create(Today)
+}
+
+Ticker<-NULL
+while(1){
+  Time<-substr(Sys.time(),12,20)
+  Ticker<-rbind(Ticker,ticker("btc_mxn"))  
+  dim<-dim(Ticker)[1]
+  if(dim>3600){
+    Ticker<-Ticker[(dim-3600):dim,]
+  }
+  if(substr(Time,4,8)=="00:00"){
+    write.csv(Ticker, paste0(Today,"/",Time,".csv")) 
+  }
+  Sys.sleep(1)
+}
+
+
+
+
+
+
+
+
 
